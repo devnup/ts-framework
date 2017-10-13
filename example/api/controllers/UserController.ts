3471f9004798c35c5943cdf9160bd0ce856db62c
Luis Brito <luiseduardo14@gmail.com>
2017-10-13 17:06:21 -0300

    Docs: Todo for user model

---
+++ example/api/controllers/UserController.ts
@@ -0,0 +1,13 @@
+import User from '../models/user/User';
+import { Controller, Post } from "../../../lib/server/router/decorators";
+
+@Controller('/users')
+export default class UserController {
+
+  @Post('/')
+  static async create(req, res) {
+    // TODO: const user = await User.create({name: req.body.name, email: req.body.email});
+    throw new Error('Not implemented: "POST users/"');
+  }
+
+}
