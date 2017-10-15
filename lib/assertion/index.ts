f1d61d953619cd01f9ed36b3456e97b093a2cf50
Luis Brito <luiseduardo14@gmail.com>
2017-10-15 03:26:55 -0200

    Feat: Sentry transport for simple logger

--- lib/assertion/index.ts
+++ lib/assertion/index.ts
@@ -1,30 +1,33 @@
+// TODO
 import * as util from "util";
 
 export interface IAssertionHelper {
   (fn: (req, res) => void): void;
-
-  toBoolean(fn: Function);
 }
 
+/**
+ * Wraps function to handle assertions as promises.
+ *
+ * @param {(req, res) => void} fn The function with assertions
+ * @returns {Promise<any>}
+ */
 const AssertionHelper = function (fn: (req, res) => void) {
   return function (req, res, next) {
     try {
       const cb = (fn(req, res) as any);
       if (cb && util.isFunction(cb.catch)) {
-        return cb.then(next).catch(error => next(error));
+        return cb.then(next).catch(error => {
+          error.status = error.status || 400;
+          next(error);
+        });
       } else {
         next();
       }
       return cb;
-    } catch(error) {
+    } catch (error) {
+      console.error(error);
       next(error);
     }
-  } as any;
-};
-
-(AssertionHelper as any).toBoolean = (fn: (req, res) => void) => {
-  return function (req, res, next) {
-    return fn(req, res);
   } as any;
 };
 
