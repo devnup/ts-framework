c18a2abe71c2be8b9757e5d8cd06983993f984b0
Luis Brito <luiseduardo14@gmail.com>
2018-04-21 03:51:58 -0300

    Test: Full coverage for async middleware

---
+++ tests/server/middlewares/async.test.ts
@@ -0,0 +1,52 @@
+import * as request from 'supertest';
+import Server from '../../../lib/server';
+
+describe('lib.server.middlewares.Async', () => {
+  it('should not wrap a number', async () => {
+
+    // Initialize a simple server
+    const server = new Server({
+      port: 3333,
+      routes: {
+        get: { '/': 1 },
+      },
+    });
+
+    // Perform a simple request to get a 200 response
+    await request(server.app).get('/')
+      .expect('Content-Type', /json/)
+      .expect(500, /Async middleware cannot wrap something that is not a function/);
+  });
+
+  it('should not wrap a string', async () => {
+
+    // Initialize a simple server
+    const server = new Server({
+      port: 3333,
+      routes: {
+        get: { '/': 'blah' },
+      },
+    });
+
+    // Perform a simple request to get a 200 response
+    await request(server.app).get('/')
+      .expect('Content-Type', /json/)
+      .expect(500, /Async middleware cannot wrap something that is not a function/);
+  });
+
+  it('should not wrap a valid function', async () => {
+
+    // Initialize a simple server
+    const server = new Server({
+      port: 3333,
+      routes: {
+        get: { '/': (req, res) => res.success({ test: 'ok' }) },
+      },
+    });
+
+    // Perform a simple request to get a 200 response
+    await request(server.app).get('/')
+      .expect('Content-Type', /json/)
+      .expect(200, { test: 'ok' });
+  });
+});
